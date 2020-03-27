82cd2fdcbf58da2e6b307512af176e43cf20fbfb
Anton Sapega <antonsapega@gmail.com>
2020-03-27 17:14:27 +0300

    Deletet unnecessary files in branch

--- task_2.js
+++
@@ -1,8 +0,0 @@
-function sort(firstValue, secondValue, thirdValue) {
-    const result = [firstValue, secondValue, thirdValue].sort((prev, next) => {
-      if (prev < next) {
-        return -1
-      }
-    }).join(', ');
-    return result;
-  }
